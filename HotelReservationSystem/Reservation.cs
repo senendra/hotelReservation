0cbf2d46741d3d58ffdad3a11141fba4bcb0c6ba
Senendra <senendra.007@gmail.com>
2020-11-04 00:12:58 +0530

    [Senendra] Refactor: Updating the method to give cheapest hotel based on weekdays rates and weekends rates per day

--- HotelReservationSystem/Reservation.cs
+++ HotelReservationSystem/Reservation.cs
@@ -9,6 +9,8 @@ namespace HotelReservationSystem
         private string hotelName;
         private int weekdaysRates;
         private int weekendsRates;
+        private int noOfWeekdays = 0;
+        private int noOfWeekends = 0;
         Dictionary<string, AddHotel> dictionary = new Dictionary<string, AddHotel>();
         public void AddDetails()
         {
@@ -23,7 +25,9 @@ namespace HotelReservationSystem
         }
         public void GetCheapestHotel()
         {
-            int max = 0, min = 0;
+            int max = 0, minWeekDay = 0 , minWeekend = 0;
+            GetDays();
+            //To get minimum rate on weekdays
             foreach (var item in dictionary)
             {
                 if (max < item.Value.weekdaysRates)
@@ -35,25 +39,51 @@ namespace HotelReservationSystem
             {
                 if (max > item.Value.weekdaysRates)
                 {
-                    min = item.Value.weekdaysRates;
+                    minWeekDay = item.Value.weekdaysRates;
+                }
+            }
+            //To get minimum rate on weekends
+            max = 0;
+            foreach (var item in dictionary)
+            {
+                if (max < item.Value.weekdaysRates)
+                {
+                    max = item.Value.weekdaysRates;
                 }
             }
             foreach (var item in dictionary)
             {
-                if (min == item.Value.weekdaysRates)
+                if (max > item.Value.weekendsRates)
                 {
-                    double sum = min * GetDays();
+                    minWeekend = item.Value.weekendsRates;
+                }
+            }
+            foreach (var item in dictionary)
+            {
+                if (minWeekDay == item.Value.weekdaysRates || minWeekend == item.Value.weekendsRates)
+                {
+                    double sum = minWeekDay * noOfWeekdays + minWeekend * noOfWeekends;
                     Console.WriteLine("Hotel : " + item.Key + " Total rates: " + sum);
                 }
             }
         }
-        public double GetDays()
+        public void GetDays()
         {
             Console.WriteLine("Enter checkin date:");
             DateTime date1 = Convert.ToDateTime(Console.ReadLine());
+            string day1 = date1.DayOfWeek.ToString(); ;
             Console.WriteLine("Enter checkout date:");
             DateTime date2 = Convert.ToDateTime(Console.ReadLine());
-            return 2;
+            if(day1 == "Saturday" || day1 == "Sunday")
+            {
+                noOfWeekdays = 1;
+                noOfWeekends = 1;
+            }
+            else
+            {
+                noOfWeekdays = 2;
+                noOfWeekends = 0;
+            }
         }
     }
 }
