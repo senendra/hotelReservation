f97fce91094394b8209a91ca0589f3f6c21f57f8
Senendra <senendra.007@gmail.com>
2020-11-03 23:36:26 +0530

    [Senendra] Add: Adding method to get cheapest hotel

--- HotelReservationSystem/Reservation.cs
+++ HotelReservationSystem/Reservation.cs
@@ -18,5 +18,39 @@ namespace HotelReservationSystem
             AddHotel add = new AddHotel(hotelName, rates);
             dictionary.Add(hotelName, add);
         }
+        public void GetCheapestHotel()
+        {
+            int max = 0, min = 0;
+            foreach (var item in dictionary)
+            {
+                if (max < item.Value.rates)
+                {
+                    max = item.Value.rates;
+                }
+            }
+            foreach (var item in dictionary)
+            {
+                if (max > item.Value.rates)
+                {
+                    min = item.Value.rates;
+                }
+            }
+            foreach (var item in dictionary)
+            {
+                if (min == item.Value.rates)
+                {
+                    double sum = min * GetDays();
+                    Console.WriteLine("Hotel : " + item.Key + " Total rates: " + sum);
+                }
+            }
+        }
+        public double GetDays()
+        {
+            Console.WriteLine("Enter checkin date:");
+            DateTime date1 = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Enter checkout date:");
+            DateTime date2 = Convert.ToDateTime(Console.ReadLine());
+            return 2;
+        }
     }
 }
