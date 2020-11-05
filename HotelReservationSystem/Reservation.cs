434cbbbc0f32229d713c333c64e21280eb3e5dfb
Senendra <senendra.007@gmail.com>
2020-11-06 00:48:52 +0530

    [Senendra] Refactor: Updated the wrong functioning methods

--- HotelReservationSystem/Reservation.cs
+++ HotelReservationSystem/Reservation.cs
@@ -9,8 +9,7 @@ namespace HotelReservationSystem
         private string hotelName;
         private int weekdaysRates;
         private int weekendsRates;
-        private int noOfWeekdays = 0;
-        private int noOfWeekends = 0;
+        private int check;
         private int rating;
         Dictionary<string, AddHotel> dictionary = new Dictionary<string, AddHotel>();
         public void AddDetails()
@@ -28,7 +27,7 @@ namespace HotelReservationSystem
         }
         public void GetCheapestHotel()
         {
-            int max = 0, minWeekDay = 0 , minWeekend = 0;
+            int max = 0, minWeekDay = 0 , minWeekend = 0, maxRating =0, totalRate;
             GetDays();
             //To get minimum rate on weekdays
             foreach (var item in dictionary)
@@ -44,14 +43,14 @@ namespace HotelReservationSystem
                 {
                     minWeekDay = item.Value.weekdaysRates;
                 }
-            }
+            }           
             //To get minimum rate on weekends
             max = 0;
             foreach (var item in dictionary)
             {
-                if (max < item.Value.weekdaysRates)
+                if (max < item.Value.weekendsRates)
                 {
-                    max = item.Value.weekdaysRates;
+                    max = item.Value.weekendsRates;
                 }
             }
             foreach (var item in dictionary)
@@ -61,13 +60,68 @@ namespace HotelReservationSystem
                     minWeekend = item.Value.weekendsRates;
                 }
             }
-            foreach (var item in dictionary)
+            ////To get best rated hotel
+            //foreach (var item in dictionary)
+            //{
+            //    if (minWeekDay == item.Value.weekdaysRates || minWeekend == item.Value.weekendsRates)
+            //    {
+            //        if(rating < item.Value.rating)
+            //        {
+            //            rating = item.Value.rating;
+            //        }
+            //    }
+            //}
+            switch (check)
             {
-                if (minWeekDay == item.Value.weekdaysRates || minWeekend == item.Value.weekendsRates)
-                {
-                    double sum = minWeekDay * noOfWeekdays + minWeekend * noOfWeekends;
-                    Console.WriteLine("Hotel : " + item.Key + " Total rates: " + sum);
-                }
+                case 1:
+                    totalRate = minWeekend * 2;
+                    foreach (var item in dictionary)
+                    {
+                        if (minWeekend == item.Value.weekendsRates)
+                        {
+                            Console.WriteLine("Hotel : " + item.Key + " Total rates: " + totalRate);
+                        }
+                    }
+                    break;
+                case 3:
+                    totalRate = minWeekDay * 2;
+                    foreach (var item in dictionary)
+                    {
+                        if (minWeekDay == item.Value.weekdaysRates)
+                        {
+                            Console.WriteLine("Hotel : " + item.Key + " Total rates: " + totalRate);
+                        }
+                    }
+                    break;
+                case 2:
+                    int sum1 = 0, sum2 = 0;
+                    string hotel1 = null, hotel2 = null;
+                    foreach (var item in dictionary)
+                    {
+                        if (minWeekDay == item.Value.weekdaysRates)
+                        {
+                            sum1 = minWeekDay + item.Value.weekendsRates;
+                            hotel1 = item.Key;
+                        }
+                        if (minWeekend == item.Value.weekendsRates)
+                        {
+                            sum2 = minWeekend + item.Value.weekdaysRates;
+                            hotel2 = item.Key;
+                        }
+                    }
+                    if (sum1 == sum2)
+                    {
+                        Console.WriteLine("Hotel : " + hotel1 +" and "+ hotel2 + " Total rates: " + sum1);
+                    }
+                    else if (sum1 > sum2)
+                    {
+                        Console.WriteLine("Hotel : " + hotel2 + " Total rates: " + sum2);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hotel : " + hotel1 + " Total rates: " + sum1);
+                    }
+                    break;
             }
         }
         public void GetDays()
@@ -77,15 +131,18 @@ namespace HotelReservationSystem
             string day1 = date1.DayOfWeek.ToString(); ;
             Console.WriteLine("Enter checkout date:");
             DateTime date2 = Convert.ToDateTime(Console.ReadLine());
-            if (day1 == "Saturday" || day1 == "Sunday")
+            string day2 = date2.DayOfWeek.ToString();
+            if (day1 == "Saturday" && day2 == "Sunday")
             {
-                noOfWeekdays = 1;
-                noOfWeekends = 1;
+                check = 1;
+            }
+            else if ((day1 == "Friday" && day2 == "Saturday") || (day1 == "Sunday" && day2 == "Monday"))
+            {
+                check = 2;
             }
             else
             {
-                noOfWeekdays = 2;
-                noOfWeekends = 0;
+                check = 3;
             }
         }
     }
