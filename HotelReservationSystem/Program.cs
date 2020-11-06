63214355d41a44adfc7523af7eb2645164bcb046
Senendra <senendra.007@gmail.com>
2020-11-07 01:03:04 +0530

    [Senendra] Refactor: finding cheapest best rated hotel for rewarded customer

--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -7,10 +7,25 @@ namespace HotelReservationSystem
         {
             Console.WriteLine("Welcome to Hotel Reservation Program");
             Reservation reservation = new Reservation();
-            reservation.AddDetailsRegular();
-            reservation.GetCheapestBestRatedHotel();
-            reservation.GetBestRatedHotel();
-            reservation.AddDetailsRewarded();
+            Console.WriteLine("Enter customer type (REGULAR/REWARDED) :");
+            string customerType = Console.ReadLine();
+            customerType = customerType.ToUpper();
+            if (customerType.Equals("REGULAR"))
+            {
+                reservation.AddDetailsRegular();
+                reservation.GetCheapestBestRatedHotel();
+                reservation.GetBestRatedHotel();
+            }
+            else if (customerType.Equals("REWARDED"))
+            {
+                reservation.AddDetailsRewarded();
+                reservation.GetCheapestBestRatedHotel();
+                reservation.GetBestRatedHotel();
+            }
+            else
+            {
+                Console.WriteLine("Your entered invalid customer type. Try Again!!");
+            }
         }
     }
 }
